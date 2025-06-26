2b3536d4eab8a0ce62af42e1b570cb2ecb7098dc
Alan Davidson <alan@viam.com>
2025-06-26 16:52:09 -0400

    I'm using an outdated version of the CLI: update the Validate() interface

--- cmd/module/main.go
+++ cmd/module/main.go
@@ -9,5 +9,5 @@ import (
 
 func main() {
 	// ModularMain can take multiple APIModel arguments, if your module implements multiple models.
-	module.ModularMain(resource.APIModel{ vision.API, detectionstosegments.DetectionsToSegments})
+	module.ModularMain(resource.APIModel{vision.API, detectionstosegments.DetectionsToSegments})
 }
