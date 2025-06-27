f1884fbb3468ca81b6f37c26e76a85defb9f1721
Alan Davidson <alan@viam.com>
2025-06-27 11:04:17 -0400

    move implementation from rdk

--- module.go
+++ module.go
@@ -1,3 +1,5 @@
+// Package detectionstosegments uses a 2D segmenter and a camera that can project its images
+// to 3D to project the bounding boxes to 3D in order to created a segmented point cloud.
 package detectionstosegments
 
 import (
@@ -5,133 +7,73 @@ import (
 	"image"
 
 	"github.com/pkg/errors"
+	"go.opencensus.io/trace"
+
+	"go.viam.com/rdk/components/camera"
 	"go.viam.com/rdk/logging"
 	"go.viam.com/rdk/resource"
+	"go.viam.com/rdk/robot"
 	"go.viam.com/rdk/services/vision"
-	vis "go.viam.com/rdk/vision"
-	"go.viam.com/rdk/vision/classification"
-	objdet "go.viam.com/rdk/vision/objectdetection"
-	"go.viam.com/rdk/vision/viscapture"
-	"go.viam.com/utils/rpc"
+	"go.viam.com/rdk/utils"
+	"go.viam.com/rdk/vision/objectdetection"
+	"go.viam.com/rdk/vision/segmentation"
 )
+
 
 var (
 	DetectionsToSegments = resource.NewModel("viam", "vision", "detections-to-segments")
-	errUnimplemented     = errors.New("unimplemented")
 )
 
 func init() {
-	resource.RegisterService(vision.API, DetectionsToSegments,
-		resource.Registration[vision.Service, *Config]{
-			Constructor: newDetectionsToSegmentsDetectionsToSegments,
-		},
-	)
-}
-
-type Config struct {
-	/*
-		Put config attributes here. There should be public/exported fields
-		with a `json` parameter at the end of each attribute.
-
-		Example config struct:
-			type Config struct {
-				Pin   string `json:"pin"`
-				Board string `json:"board"`
-				MinDeg *float64 `json:"min_angle_deg,omitempty"`
+	resource.RegisterService(vision.API, DetectionsToSegments, resource.Registration[vision.Service, *segmentation.DetectionSegmenterConfig]{
+		DeprecatedRobotConstructor: func(
+			ctx context.Context, r any, c resource.Config, logger logging.Logger,
+		) (vision.Service, error) {
+			attrs, err := resource.NativeConfig[*segmentation.DetectionSegmenterConfig](c)
+			if err != nil {
+				return nil, err
 			}
-
-		If your model does not need a config, replace *Config in the init
-		function with resource.NoNativeConfig
-	*/
+			actualR, err := utils.AssertType[robot.Robot](r)
+			if err != nil {
+				return nil, err
+			}
+			return register3DSegmenterFromDetector(ctx, c.ResourceName(), attrs, actualR)
+		},
+	})
 }
 
-// Validate ensures all parts of the config are valid and important fields exist.
-// Returns implicit dependencies based on the config.
-// The path is the JSON path in your robot's config (not the `Config` struct) to the
-// resource being validated; e.g. "components.0".
-func (cfg *Config) Validate(path string) ([]string, []string, error) {
-	// Add config validation code here
-	return nil, nil, nil
-}
-
-type detectionsToSegmentsDetectionsToSegments struct {
-	resource.AlwaysRebuild
-
-	name resource.Name
-
-	logger logging.Logger
-	cfg    *Config
-
-	cancelCtx  context.Context
-	cancelFunc func()
-}
-
-func newDetectionsToSegmentsDetectionsToSegments(ctx context.Context, deps resource.Dependencies, rawConf resource.Config, logger logging.Logger) (vision.Service, error) {
-	conf, err := resource.NativeConfig[*Config](rawConf)
+// register3DSegmenterFromDetector creates a 3D segmenter from a previously registered detector.
+func register3DSegmenterFromDetector(
+	ctx context.Context,
+	name resource.Name,
+	conf *segmentation.DetectionSegmenterConfig,
+	r robot.Robot,
+) (vision.Service, error) {
+	_, span := trace.StartSpan(ctx, "service::vision::register3DSegmenterFromDetector")
+	defer span.End()
+	if conf == nil {
+		return nil, errors.New("config for 3D segmenter made from a detector cannot be nil")
+	}
+	detectorService, err := vision.FromRobot(r, conf.DetectorName)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrapf(err, "could not find necessary dependency, detector %q", conf.DetectorName)
 	}
-
-	return NewDetectionsToSegments(ctx, deps, rawConf.ResourceName(), conf, logger)
-
-}
-
-func NewDetectionsToSegments(ctx context.Context, deps resource.Dependencies, name resource.Name, conf *Config, logger logging.Logger) (vision.Service, error) {
-
-	cancelCtx, cancelFunc := context.WithCancel(context.Background())
-
-	s := &detectionsToSegmentsDetectionsToSegments{
-		name:       name,
-		logger:     logger,
-		cfg:        conf,
-		cancelCtx:  cancelCtx,
-		cancelFunc: cancelFunc,
+	confThresh := 0.5 // default value
+	if conf.ConfidenceThresh > 0.0 {
+		confThresh = conf.ConfidenceThresh
 	}
-	return s, nil
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) Name() resource.Name {
-	return s.name
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) NewClientFromConn(ctx context.Context, conn rpc.ClientConn, remoteName string, name resource.Name, logger logging.Logger) (vision.Service, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) DetectionsFromCamera(ctx context.Context, cameraName string, extra map[string]interface{}) ([]objdet.Detection, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) Detections(ctx context.Context, img image.Image, extra map[string]interface{}) ([]objdet.Detection, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) ClassificationsFromCamera(ctx context.Context, cameraName string, n int, extra map[string]interface{}) (classification.Classifications, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) Classifications(ctx context.Context, img image.Image, n int, extra map[string]interface{}) (classification.Classifications, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) GetObjectPointClouds(ctx context.Context, cameraName string, extra map[string]interface{}) ([]*vis.Object, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) GetProperties(ctx context.Context, extra map[string]interface{}) (*vision.Properties, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) CaptureAllFromCamera(ctx context.Context, cameraName string, captureOptions viscapture.CaptureOptions, extra map[string]interface{}) (viscapture.VisCapture, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) DoCommand(ctx context.Context, cmd map[string]interface{}) (map[string]interface{}, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) Close(context.Context) error {
-	// Put close code here
-	s.cancelFunc()
-	return nil
+	detector := func(ctx context.Context, img image.Image) ([]objectdetection.Detection, error) {
+		return detectorService.Detections(ctx, img, nil)
+	}
+	segmenter, err := segmentation.DetectionSegmenter(objectdetection.Detector(detector), conf.MeanK, conf.Sigma, confThresh)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot create 3D segmenter from detector")
+	}
+	if conf.DefaultCamera != "" {
+		_, err = camera.FromRobot(r, conf.DefaultCamera)
+		if err != nil {
+			return nil, errors.Errorf("could not find camera %q", conf.DefaultCamera)
+		}
+	}
+	return vision.NewService(name, r, nil, nil, detector, segmenter, conf.DefaultCamera)
 }
