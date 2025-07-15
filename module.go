8e246cf5d962597aaccf84a2a436d5e0e8493c27
Adwait Ganguly <69320559+AGanguly13@users.noreply.github.com>
2025-07-15 16:51:28 -0400

    [RSDK-10922] move detections_to_segments from RDK to Go module (#2)
    * move implementation from rdk
    
    * copy tests from rdk
    
    * go mod tidy
    
    * implementation of the module
    
    * move over config object file from rdk
    
    * mod and sum files
    
    * cleans up implemntation
    
    * make test file conform to new implementation
    
    * readme progress
    
    * read me done
    
    * meta.json complete
    
    * pr feedback, migrated detection_to_object to module, named imports
    
    * small edits
    
    * changes for darwin cloud build
    
    ---------
    
    Co-authored-by: Alan Davidson <alan@viam.com>
    Co-authored-by: AGanguly13 <adwaitganguly@viam.com>

--- module.go
+++ module.go
@@ -1,137 +1,241 @@
+// Package detectionstosegments uses a 2D segmenter and a camera that can project its images
+// to 3D to project the bounding boxes to 3D in order to created a segmented point cloud.
 package detectionstosegments
 
 import (
 	"context"
 	"image"
 
+	"github.com/go-viper/mapstructure/v2"
 	"github.com/pkg/errors"
+	"go.opencensus.io/trace"
+	"go.viam.com/rdk/components/camera"
 	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/pointcloud"
 	"go.viam.com/rdk/resource"
-	"go.viam.com/rdk/services/vision"
-	vis "go.viam.com/rdk/vision"
-	"go.viam.com/rdk/vision/classification"
-	objdet "go.viam.com/rdk/vision/objectdetection"
-	"go.viam.com/rdk/vision/viscapture"
-	"go.viam.com/utils/rpc"
+	"go.viam.com/rdk/rimage"
+	"go.viam.com/rdk/rimage/transform"
+	servicevision "go.viam.com/rdk/services/vision"
+	"go.viam.com/rdk/spatialmath"
+	"go.viam.com/rdk/utils"
+	vision "go.viam.com/rdk/vision"
+	"go.viam.com/rdk/vision/objectdetection"
+	"go.viam.com/rdk/vision/segmentation"
 )
 
-var (
-	DetectionsToSegments = resource.NewModel("viam", "vision", "detections-to-segments")
-	errUnimplemented     = errors.New("unimplemented")
-)
+var DetectionsToSegments = resource.NewModel("viam", "vision", "detections-to-segments")
+
+// DetectionSegmenterConfig are the optional parameters to turn a detector into a segmenter.
+type DetectionSegmenterConfig struct {
+	DetectorName     string  `json:"detector_name"`
+	ConfidenceThresh float64 `json:"confidence_threshold_pct"`
+	MeanK            int     `json:"mean_k"`
+	Sigma            float64 `json:"sigma"`
+	DefaultCamera    string  `json:"camera_name"`
+}
 
 func init() {
-	resource.RegisterService(vision.API, DetectionsToSegments,
-		resource.Registration[vision.Service, *Config]{
-			Constructor: newDetectionsToSegmentsDetectionsToSegments,
-		},
-	)
-}
-
-type Config struct {
-	/*
-		Put config attributes here. There should be public/exported fields
-		with a `json` parameter at the end of each attribute.
-
-		Example config struct:
-			type Config struct {
-				Pin   string `json:"pin"`
-				Board string `json:"board"`
-				MinDeg *float64 `json:"min_angle_deg,omitempty"`
+	resource.RegisterService(servicevision.API, DetectionsToSegments, resource.Registration[servicevision.Service, *DetectionSegmenterConfig]{
+		Constructor: func(
+			ctx context.Context, deps resource.Dependencies, c resource.Config, logger logging.Logger,
+		) (servicevision.Service, error) {
+			attrs, err := resource.NativeConfig[*DetectionSegmenterConfig](c)
+			if err != nil {
+				return nil, err
 			}
-
-		If your model does not need a config, replace *Config in the init
-		function with resource.NoNativeConfig
-	*/
+			return register3DSegmenterFromDetector(ctx, c.ResourceName(), attrs, deps, logger)
+		},
+	})
 }
 
-// Validate ensures all parts of the config are valid and important fields exist.
-// Returns implicit dependencies based on the config.
-// The path is the JSON path in your robot's config (not the `Config` struct) to the
-// resource being validated; e.g. "components.0".
-func (cfg *Config) Validate(path string) ([]string, []string, error) {
-	// Add config validation code here
-	return nil, nil, nil
+// register3DSegmenterFromDetector creates a 3D segmenter from a previously registered detector.
+func register3DSegmenterFromDetector(
+	ctx context.Context,
+	name resource.Name,
+	conf *DetectionSegmenterConfig,
+	deps resource.Dependencies,
+	logger logging.Logger,
+) (servicevision.Service, error) {
+	_, span := trace.StartSpan(ctx, "service::vision::register3DSegmenterFromDetector")
+	defer span.End()
+	if conf == nil {
+		return nil, errors.New("config for 3D segmenter made from a detector cannot be nil")
+	}
+	detectorService, err := servicevision.FromDependencies(deps, conf.DetectorName)
+	if err != nil {
+		return nil, errors.Wrapf(err, "could not find necessary dependency, detector %q", conf.DetectorName)
+	}
+	confThresh := 0.5 // default value
+	if conf.ConfidenceThresh > 0.0 {
+		confThresh = conf.ConfidenceThresh
+	}
+	detector := func(ctx context.Context, img image.Image) ([]objectdetection.Detection, error) {
+		return detectorService.Detections(ctx, img, nil)
+	}
+	segmenter, err := DetectionSegmenter(objectdetection.Detector(detector), conf.MeanK, conf.Sigma, confThresh)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot create 3D segmenter from detector")
+	}
+	if conf.DefaultCamera != "" {
+		_, err = camera.FromDependencies(deps, conf.DefaultCamera)
+		if err != nil {
+			return nil, errors.Errorf("could not find camera %q", conf.DefaultCamera)
+		}
+	}
+	return servicevision.NewService(name, deps, logger, nil, nil, detector, segmenter, conf.DefaultCamera)
 }
 
-type detectionsToSegmentsDetectionsToSegments struct {
-	resource.AlwaysRebuild
+func (conf *DetectionSegmenterConfig) Validate(path string) ([]string, []string, error) {
+	var requiredDeps []string
+	var optionalDeps []string
 
-	name resource.Name
+	if conf.DefaultCamera != "" {
+		requiredDeps = append(requiredDeps, conf.DefaultCamera)
+	}
 
-	logger logging.Logger
-	cfg    *Config
+	if conf.DetectorName == "" {
+		return nil, optionalDeps, errors.Errorf("expected a detector to be specified")
+	}
+	requiredDeps = append(requiredDeps, conf.DetectorName)
 
-	cancelCtx  context.Context
-	cancelFunc func()
+	if conf.MeanK <= 0 {
+		return nil, optionalDeps, errors.Errorf("expected a mean k to be specified")
+	}
+
+	if conf.Sigma <= 0 {
+		return nil, optionalDeps, errors.Errorf("expected a sigma to be specified")
+	}
+
+	return requiredDeps, optionalDeps, nil
 }
 
-func newDetectionsToSegmentsDetectionsToSegments(ctx context.Context, deps resource.Dependencies, rawConf resource.Config, logger logging.Logger) (vision.Service, error) {
-	conf, err := resource.NativeConfig[*Config](rawConf)
+// ConvertAttributes changes the AttributeMap input into a DetectionSegmenterConfig.
+func (dsc *DetectionSegmenterConfig) ConvertAttributes(am utils.AttributeMap) error {
+	decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{TagName: "json", Result: dsc})
+	if err != nil {
+		return err
+	}
+	return decoder.Decode(am)
+}
+
+func cameraToProjector(
+	ctx context.Context,
+	source camera.Camera,
+) (transform.Projector, error) {
+	if source == nil {
+		return nil, errors.New("cannot have a nil source")
+	}
+	props, err := source.Properties(ctx)
+	if err != nil {
+		return nil, camera.NewPropertiesError("source camera")
+	}
+	if props.IntrinsicParams == nil {
+		return &transform.ParallelProjection{}, nil
+	}
+	cameraModel := transform.PinholeCameraModel{}
+	cameraModel.PinholeCameraIntrinsics = props.IntrinsicParams
+
+	if props.DistortionParams != nil {
+		cameraModel.Distortion = props.DistortionParams
+	}
+
+	return &cameraModel, nil
+}
+
+// DetectionSegmenter will take an objectdetector.Detector and turn it into a Segementer.
+// The params for the segmenter are "mean_k" and "sigma" for the statistical filter on the point clouds.
+func DetectionSegmenter(detector objectdetection.Detector, meanK int, sigma, confidenceThresh float64) (segmentation.Segmenter, error) {
+	var err error
+	if detector == nil {
+		return nil, errors.New("detector cannot be nil")
+	}
+	var filter func(in, out pointcloud.PointCloud) error
+	if meanK > 0 && sigma > 0.0 {
+		filter, err = pointcloud.StatisticalOutlierFilter(meanK, sigma)
+		if err != nil {
+			return nil, err
+		}
+	}
+	// return the segmenter
+	seg := func(ctx context.Context, src camera.Camera) ([]*vision.Object, error) {
+		proj, err := cameraToProjector(ctx, src)
+		if err != nil {
+			return nil, err
+		}
+		// get the 3D detections, and turn them into 2D image and depthmap
+		imgs, _, err := src.Images(ctx)
+		if err != nil {
+			return nil, errors.Wrapf(err, "detection segmenter")
+		}
+		var img *rimage.Image
+		var dmimg image.Image
+		for _, i := range imgs {
+			thisI := i
+			if i.SourceName == "color" {
+				img = rimage.ConvertImage(thisI.Image)
+			}
+			if i.SourceName == "depth" {
+				dmimg = thisI.Image
+			}
+		}
+		if img == nil || dmimg == nil {
+			return nil, errors.New("source camera's getImages method did not have 'color' and 'depth' images")
+		}
+		dm, err := rimage.ConvertImageToDepthMap(ctx, dmimg)
+		if err != nil {
+			return nil, err
+		}
+		im := rimage.CloneImage(img)
+		dets, err := detector(ctx, im) // detector may modify the input image
+		if err != nil {
+			return nil, err
+		}
+
+		objects := make([]*vision.Object, 0, len(dets))
+		for _, d := range dets {
+			if d.Score() < confidenceThresh {
+				continue
+			}
+			// TODO(bhaney): Is there a way to just project the detection boxes themselves?
+			pc, err := detectionToPointCloud(d, img, dm, proj)
+			if err != nil {
+				return nil, err
+			}
+			if filter != nil {
+				out := pc.CreateNewRecentered(spatialmath.NewZeroPose())
+				err = filter(pc, out)
+				if err != nil {
+					return nil, err
+				}
+				pc = out
+			}
+			// if object was filtered away, skip it
+			if pc.Size() == 0 {
+				continue
+			}
+			obj, err := vision.NewObjectWithLabel(pc, d.Label(), nil)
+			if err != nil {
+				return nil, err
+			}
+			objects = append(objects, obj)
+		}
+		return objects, nil
+	}
+	return seg, nil
+}
+
+func detectionToPointCloud(
+	d objectdetection.Detection,
+	im *rimage.Image, dm *rimage.DepthMap,
+	proj transform.Projector,
+) (pointcloud.PointCloud, error) {
+	bb := d.BoundingBox()
+	if bb == nil {
+		return nil, errors.New("detection bounding box cannot be nil")
+	}
+	pc, err := proj.RGBDToPointCloud(im, dm, *bb)
 	if err != nil {
 		return nil, err
 	}
-
-	return NewDetectionsToSegments(ctx, deps, rawConf.ResourceName(), conf, logger)
-
-}
-
-func NewDetectionsToSegments(ctx context.Context, deps resource.Dependencies, name resource.Name, conf *Config, logger logging.Logger) (vision.Service, error) {
-
-	cancelCtx, cancelFunc := context.WithCancel(context.Background())
-
-	s := &detectionsToSegmentsDetectionsToSegments{
-		name:       name,
-		logger:     logger,
-		cfg:        conf,
-		cancelCtx:  cancelCtx,
-		cancelFunc: cancelFunc,
-	}
-	return s, nil
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) Name() resource.Name {
-	return s.name
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) NewClientFromConn(ctx context.Context, conn rpc.ClientConn, remoteName string, name resource.Name, logger logging.Logger) (vision.Service, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) DetectionsFromCamera(ctx context.Context, cameraName string, extra map[string]interface{}) ([]objdet.Detection, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) Detections(ctx context.Context, img image.Image, extra map[string]interface{}) ([]objdet.Detection, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) ClassificationsFromCamera(ctx context.Context, cameraName string, n int, extra map[string]interface{}) (classification.Classifications, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) Classifications(ctx context.Context, img image.Image, n int, extra map[string]interface{}) (classification.Classifications, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) GetObjectPointClouds(ctx context.Context, cameraName string, extra map[string]interface{}) ([]*vis.Object, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) GetProperties(ctx context.Context, extra map[string]interface{}) (*vision.Properties, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) CaptureAllFromCamera(ctx context.Context, cameraName string, captureOptions viscapture.CaptureOptions, extra map[string]interface{}) (viscapture.VisCapture, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) DoCommand(ctx context.Context, cmd map[string]interface{}) (map[string]interface{}, error) {
-	panic("not implemented")
-}
-
-func (s *detectionsToSegmentsDetectionsToSegments) Close(context.Context) error {
-	// Put close code here
-	s.cancelFunc()
-	return nil
+	return pc, nil
 }
